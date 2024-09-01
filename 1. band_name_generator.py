b86de9d5cec1c65f2360862e70ad600d3f694f7d
Wishmi Sasika <88582109+Wishmi-Sasika@users.noreply.github.com>
2024-09-02 01:02:10 +0530

    Create 1. band_name_generator.py

---
+++ 1. band_name_generator.py
@@ -0,0 +1,9 @@
+print("WELCOME TO THE BAND NAME GENERATOR")
+
+print("Your band name is " + input("what is the name of your hometown?") + " " +  input("What is the name of your pet?"))
+
+hometown = input("what is the name of your hometown?")
+print(hometown)
+pet = input("what is the name of your pet?")
+print(pet)
+print("Name of your band : " + hometown + " " + pet)
